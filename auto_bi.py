39a7891b27e0d90aebd6a6804ecb61f52d63cd65
AMAURY <amaurylejay@orange.fr>
2020-10-22 21:17:32 +0200

    added timestamp cleaned functions

--- auto_bi.py
+++ auto_bi.py
@@ -7,6 +7,10 @@ import seaborn as sns
 import random 
 import warnings
 
+# timestamp
+from dateutil import parser as dateparser # doc https://dateutil.readthedocs.io/en/stable/parser.html
+from dateutil.parser import parserinfo as dateparser_info
+
 # Visualization
 import plotly.express as px
 import dash
@@ -25,7 +29,7 @@ from box import Box # uselful to access yaml files like this config.group.subgro
 
 
 # TODO: _display_basic_kpi + compare pandas profilling
-# TODO: imprive handling of missing values / Replace missing values strat? 
+# TODO: improve handling of missing values / Replace missing values strat? 
 # TODO: Handle case where there isn't any numerical or cat features 
 # TODO: Decide to potentially remove the features that are too correlated
 # TODO: Handle case where the main ID column for instance is changed, the functions that depend on its value need all to be re-run DONE 
@@ -46,12 +50,193 @@ from box import Box # uselful to access yaml files like this config.group.subgro
 # TODO: Decide how many features we choose for text
 # TODO: Feature extractor (text) DONE
 
+class timestamp_handling(categorical_target_variable):
+	"""Class gathering the functions that handles datasets with timestamps"""
+
+	def __init__(self, dataframe, target_variable, timestamp_column):
+		super().__init__(dataframe, target_variable, timestamp_column)
+
+	def timestamp_processing(self, list_granularity = ['year','month','weekday','hour','minute']):
+	    """
+	    Convert the date column as a timestamp if it's not as such already. Then create a year, month, weekday, hour, minute columns
+	    input: 
+	        - df : pandas dataframe
+	        - timestamp_column : string 
+	    output:
+	        - df : pandas dataframe with the new columns
+	    """
+	    
+	    # Checking if the date column is in the right date format or if it needs to be processed
+	    if str(self.df[timestamp_column].dtype) != "datetime64[ns]":
+	        self.df[timestamp_column] = self.df.apply(lambda row: dateparser.parse(row[timestamp_column], parserinfo=date_parser_info, dayfirst = False),axis = 1)
+	    
+	    # Creating the date only column, without timestamps
+	    self.df['date_only'] = self.df[timestamp_column].apply(lambda row: row.date())
+	    
+	    # Creating column to isolate specific timestamps
+	    if "year" in list_granularity: 
+	        self.df['year'] = self.df[timestamp_column].apply(lambda row: row.year)
+	    if "month" in list_granularity: 
+	        self.df['month'] = self.df[timestamp_column].apply(lambda row: row.month)
+	    if "weekday" in list_granularity: 
+	        self.df['weekday'] = self.df[timestamp_column].apply(lambda row: row.weekday())
+	        self.df['weekend'] = 0
+	        self.df.loc[self.df.weekday > 4, 'weekend'] = 1
+	    if "hour" in list_granularity: 
+	        self.df['hour'] = self.df[timestamp_column].apply(lambda row: row.hour)
+	    if "minute" in list_granularity:
+	        self.df['minute'] = self.df[timestamp_column].apply(lambda row: row.minute)
+	    
+	    return self.df
+
+	def identify_ideal_granularity_visualisation(df, min_threshold_dic):
+	    """Check for each granularity level of time (year, month, day etc) how much is present in the dataset
+	    input : dataframe, minimum threshold dictionnary
+	    output : dictionnary such as the one below
+	        {'year': {'consecutive_nbr': 5, 'ratio': 1},
+	         'month': {'consecutive_nbr': 12, 'ratio': 1.0},
+	         'weekday': {'consecutive_nbr': 5, 'ratio': 0.7142857142857143}}
+	         
+	         If the scale element doesn't meet the required threshold set in the min_threshold_dic (i.e. hour in the example above)
+	         then it won't appear in the output dictionnary """
+	    
+	    list_granularity_option = ['year','month','weekday','hour']# ,'seconds','other',
+	    scales = {}
+	    
+	    def _check_cycle_threshold(df, element, scales):
+	        """Build for a given scale (week, month, hour etc) the ratio of represented elements (i.e. 4 months out of 12, 5 days out of 7 etc)
+	        and check if that ratio passes a given threshold
+	        input: 
+	            - dataframe
+	            - string - element (year, month, day, hour ...)
+	            - dictionnary - scales"""
+	        
+	        def _check_nbr_consecutive_elements(nbr_consecutive_elements, scale, scales):
+	            """calculate the ratio and add it to the output dictionnary scale
+	            input: nbr of consecutive month, days etc
+	            output: dictionnary - scale updated with ratio information if the minimum threshold is passed 
+	            """
+	            if nbr_consecutive_elements >= min_threshold_dic[scale]['threshold']:
+	            
+	                ratio = float(nbr_consecutive_elements/min_threshold_dic[scale]['max'])
+	                scales[scale] = {'consecutive_nbr':nbr_consecutive_elements, 'ratio':ratio}
+
+	            return scales 
+	        
+	        list_unique_elements = sorted(df[element].unique()) #HERE
+	        nbr_consecutive_elements = 0
+
+	        for element_value in list_unique_elements:
+	              # Doesn't handle the case where we have 2020-01, 2020-02, 2020-03, 2020-12, will consider that 2020-12 is part of 2019, and therefore the months consecutive
+	              # TODO : change that HERE  
+	            try: 
+	                # Checking that the two months are consecutive
+	                if element_value+1 == list_unique_elements[list_unique_elements.index(element_value)+1]: #HERE
+	                    nbr_consecutive_elements +=1
+	                # restarting the counter and keep searching for consecutive months
+	                else:
+	                    nbr_consecutive_elements = 0
+
+	            # Excluding the last month that cause an out of range error
+	            except IndexError: 
+	                nbr_consecutive_elements +=1
+	                
+	            # Checking if at this point (not at the end of the list of unique month, days etc yet) we have enough consecutive element to validate this scale 
+	            scales = _check_nbr_consecutive_elements(nbr_consecutive_elements, element, scales)
+	        # if we managed to get to the end of the list of elements to check without issue, check that we have found enough to validate the scale
+	        scales = _check_nbr_consecutive_elements(nbr_consecutive_elements, element, scales)
+	        
+	        return scales
+	    
+	    for scale in list_granularity_option:
+	        # Check that there is a cycle (i.e. minimum of two elements - year, month, week etc)
+	        
+	        if len(df[scale].unique()) > 2:
+	            # Check that at least threshold of the cycle is represented
+	            # except for Year that doesn't require a minimum threshold (12 months make a year cycle but no minimum number of year make a cycle - assuming we don't care about decades)
+	            if scale =='year':
+	                scales['year'] = {'consecutive_nbr':len(df[scale].unique()), 'ratio':1}
+	            
+	            elif scale == 'month':
+	                _check_cycle_threshold(df, scale, scales)
+	                
+	            elif scale == 'weekday':
+	                _check_cycle_threshold(df, scale, scales)
+	                
+	            elif scale == 'hour':
+	                _check_cycle_threshold(df, scale, scales)
+	            
+	            elif scale == 'minute':
+	                _check_cycle_threshold(df, scale, scales)
+	                            
+	    return scales
+
+	def find_largest_delta(df, column_of_interest, delta = "increase", display_top_x_values = 10, rolling = 1):
+	    """ identify the periods of longest consecutive growth or decrease
+	    input:
+	        - dataframe - main dataframe
+	        - string - column of interest that has the values we want to evaluate
+	        - delta - default is increase in which case we look for period of consecutive GROWTH, set it to "decrease"
+	          to look for period of consecutive decrease.
+	        - int - display_top_x_values sets the number of values we want to have
+	        - int - rolling sets the rolling period used to calculate the mean value.
+	    output: 
+	        - list of tuples with ((index_start,index_end), delta value)
+	        [((528, 571), 72.03633333333357),
+	         ((660, 699), 40.292333333333204),
+	         ((173, 203), 31.129333333333363)] """
+	    
+	    start = 0
+	    end = 0
+	    max_start = 0 
+	    max_end = 0
+	    diff_end_start_values = 0
+	    all_consecutive_delta = {}
+	    
+	    # Setting dates index 
+	    df.set_index(date_column,inplace = True)
+	    
+	    rol_mean = df[column_of_interest].astype(float).rolling(rolling).mean() # Applying rolling mean on values to remove noise and measure longer term increase or decrease 
+	    val = [value for value in list(rol_mean) if str(value) != 'nan'] # List of the values (i.e. transpose of the column of interest)
+	    rol_mean.plot() 
+	        
+	    if delta == "decrease": # Case where we look for the longest consecutive decrease
+	        for i in range(0,len(val)-1): # iterating over all the values
+	            if val[i] > val[i+1]: # Next value is smaller, good, we keep going
+	                end = i+1
+	            else: # We are hitting a reversal point in the values, therefore we record the decrease period we just traversed
+	                diff_start_end_values = val[start] - val[end]
+	                all_consecutive_delta[(start,end)] = diff_start_end_values
+	                start = i+1 
+	                end = i+1
+
+	        # Resetting index back to the initial form
+	        df.reset_index(inplace = True)
+
+	        return sorted(all_consecutive_delta.items(), key=lambda x: x[1], reverse=True)[:display_top_x_values]
+
+	    else: # delta = "increase" default where we look for the longest consecutive decrease
+	        for i in range(0,len(val)-1): # iterating over all the values
+	            if val[i] < val[i+1]: # Next value is bigger, good, we keep going
+	                end = i+1
+	            else: # We are hitting a reversal point in the values, therefore we record the increase period we just traversed
+	                diff_end_start_values = val[end] - val[start]
+	                all_consecutive_delta[(start,end)] = diff_end_start_values
+	                start = i+1 
+	                end = i+1
+
+	        # Resetting index back to the initial form
+	        df.reset_index(inplace = True)
+
+	        return sorted(all_consecutive_delta.items(), key=lambda x: x[1], reverse=True)[:display_top_x_values]
+# TODO Launch the timestamps function in the categorical_target_variable if there is indeed a timestamp present
+
 
 class categorical_target_variable():
 	"""Main class so far, handling dataset that possess a categorical target variable to study (example famous titanic survived feature [0,1]).
 	The main function to call within it is analyse"""
 
-	def __init__(self, dataframe, target_variable, categorical_columns, numerical_col, id_col = None, config_path='./config.yml'):
+	def __init__(self, dataframe, target_variable, categorical_columns, numerical_col, id_col = None, timestamp_column = None, config_path='./config.yml'):
 		
 		# Mandatory parameters
 		self.df = dataframe # DataFrame
@@ -62,6 +247,7 @@ class categorical_target_variable():
 		# Optional parameters
 		self.config_path = config_path
 		self.id_col = id_col
+		self.timestamp_column = timestamp_column
 
 		# Will be generated throughout the analysis
 		self.df_head = None
@@ -113,8 +299,12 @@ class categorical_target_variable():
 	   
 	    missing_val_serie = self.df.isnull().sum().sort_values()
 	    
-	    # Choosing the main column to use as primary key
-	    self.id_col = missing_val_serie.index[0]
+	    # Choosing the main column to use as primary key 
+	    # Added the OPTIONAL setting of id column as it is possible that the user already manually inputed an ID column
+	    
+	    if self.id_col == None: 
+	    	self.id_col = missing_val_serie.index[0]
+
 	    number_of_missing_value_main_col = missing_val_serie[0]
 	    # Calculating missing value ratio
 	    ratio = number_of_missing_value_main_col / self.df.shape[0]
